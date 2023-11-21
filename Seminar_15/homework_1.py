317d0c351c3655aa6eb9adb44286340d4657cac3
HeroSima43 <pwd@list.ru>
2023-11-21 12:31:55 +0300

    add Seminar_15

---
+++ Seminar_15/homework_1.py
@@ -0,0 +1,67 @@
+# Семинар 1, задача 2.
+# Напишите код, который запрашивает число и сообщает является ли оно простым или составным.
+# Используйте правило для проверки: «Число является простым, если делится нацело только на единицу
+# и на себя». Сделайте ограничение на ввод отрицательных чисел и чисел больше 100 тысяч.
+
+# Добавлено к ниму логирование ошибок и полезной информации.
+# Также реализована возможность запуска из командной строки с передачей параметров.
+
+
+import logging
+import argparse
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def user_input_number(message: str, lower_limit: int, upper_limit: int) -> int:
+    """Пользовательский ввод."""
+    while True:
+        try:
+            number = int(input(f'{message}'))
+            if lower_limit <= number <= upper_limit:
+                return number
+            else:
+                logging.error(
+                    f'Число должно быть между {lower_limit} и {upper_limit}')
+        except ValueError:
+            logging.error('Неверный Ввод. Введите натуральное число.')
+
+
+def prime_number_test(number: int) -> str:
+    """Проверка на простое число."""
+    if number in (1, 2):
+        return 'Простое число'
+    for i in range(2, int(number ** 0.5) + 1):
+        if (number % i) == 0:
+            return 'Составное число'
+    return 'Простое число'
+
+
+def main(lower_limit: int, upper_limit: int, number: int = None):
+    """Основная функция для обработки пользовательского ввода и вывода результата."""
+    if number is None:
+        message_for_user = f'Введите натуральное число от {lower_limit} до {upper_limit}: '
+        number = user_input_number(message_for_user, lower_limit, upper_limit)
+    result = prime_number_test(number)
+    print(f'{result} - {number}')
+    logging.info(f'{result} - {number}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Проверьте, является ли число простым или составным.')
+    parser.add_argument('-n', '--number', type=int)
+    args = parser.parse_args()
+
+    LOWER_LIMIT = 0
+    UPPER_LIMIT = 100000
+
+    if args.number:
+        if LOWER_LIMIT <= args.number <= UPPER_LIMIT:
+            main(LOWER_LIMIT, UPPER_LIMIT, args.number)
+        else:
+            logging.error(
+                f'Число должно находиться между {LOWER_LIMIT} и {UPPER_LIMIT}.')
+    else:
+        main(LOWER_LIMIT, UPPER_LIMIT)
